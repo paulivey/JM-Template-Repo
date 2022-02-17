8d6764cea556dc8e346ebcdfb462788ada3a6907
Paul Ivey <47188956+paul-msft@users.noreply.github.com>
2022-02-17 16:19:29 +0000

    Update main.tf

--- terraform/policy/main.tf
+++ terraform/policy/main.tf
@@ -17,7 +17,7 @@ module "policy_assignment" {
   source = "git::https://github.com/iveylabs/JM-TF-Modules.git//modules/azure_policy_rg_assignment?ref=main"
 
   resource_group_name = "RG-${upper(var.environment)}-${upper(var.project)}-${upper(var.region_short)}-${upper(var.app_name)}-${var.app_suffix}"
-  tag_country         = "${upper(var.tag_country))}"
+  tag_country         = "${upper(var.tag_country)}"
   tag_environment     = "${upper(var.tag_environment)}"
   tag_window          = "${upper(var.tag_window)}"
   tag_sector          = "${upper(var.tag_sector)}"
